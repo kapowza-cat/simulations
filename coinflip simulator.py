2a800a9ba8398581c4eb6edcdb410b134a07db68
Ryan <ryan.a.ellis7@gmail.com>
2025-12-07 19:40:53 -0700

    coinflip v1.1

--- coinflip simulator.py
+++ coinflip simulator.py
@@ -1,13 +1,19 @@
 import random
 
+
 current = 0
 times = 0
+increment = 1
+target = 30
 
-while current != 20:
-    times+=1
-    if random.randint(0,1) == 0:
-     current+=1
-    else:
-     current=0
-
-print(times)
+while increment < target+1:
+    while current !=increment:
+        times+=1
+        if random.randint(0,1) == 0:
+            current+=1
+        else:
+            current=0
+    print(times , "at" , increment)
+    increment+=1
+    times = 0
+    current = 0
