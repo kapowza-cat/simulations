ef6f5f6c6034ba8ee34003b041cbdbadae1ba195
Ryan <ryan.a.ellis7@gmail.com>
2026-03-01 14:17:15 -0700

    testing csv

--- TPIR showcase simulator.py
+++ TPIR showcase simulator.py
@@ -2,11 +2,15 @@ import random
 import csv_export
 
 iterations = 5000
+spinResults = []
+currentIteration = 1
 
-def simulatorRound():
+def simulateRound():
     spin1 = random.randint(1,20)
     spin1 = spin1 * 5
-    print (spin1)
+    spinResults.append(spin1)
 
-for count in range (iterations):
-    simulatorRound()
+for i in range (iterations):
+    simulateRound()
+
+csv_export.export_to_csv("test.csv",spinResults)
